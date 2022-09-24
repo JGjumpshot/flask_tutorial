ba6eee0ffc3007c9131a9879aee2ca1efcd4b03e
Jackson Gunther <jackson.gunther@yahoo.com>
2022-09-24 13:18:49 -0600

    updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import smtplib
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,3 +11,27 @@ def index():
 def about():
     names = ["Melanie", "Kris", "Sandy", "Tucker"]
     return render_template("about.html", names=names)
+
+@app.route("/subscribe")
+def subscribe():
+    return render_template("subscribe.html")
+
+@app.route("/form", methods=["POST"])
+def form():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+
+    message = "You have been subscribed to my email newsletter"
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login("", )
+
+    if not first_name or not last_name or not email:
+        error_statement = "All form fields required"
+        return render_template("subscribe.html", error_statement= error_statement,
+        first_name=first_name,
+        last_name=last_name,
+        email=email
+        )
+    return render_template("form.html", first_name=first_name, last_name=last_name, email=email)
